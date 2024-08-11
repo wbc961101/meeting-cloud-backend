97e7036fec98110c3bb12d9c686fff65d204b664
wbc961101 <1548998651@qq.com>
2024-08-11 10:30:54 +0800

    build project

--- meeting-manage/src/main/java/com/wang/meeting/controller/HomeController.java
+++ meeting-manage/src/main/java/com/wang/meeting/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.wang.meeting.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/home")
+@Slf4j
 public class HomeController {
 
     @GetMapping("/liveness")
     public ResponseEntity<String> hello() {
-
+        log.info("debug mode");
         return ResponseEntity.ok().body("ok");
     }
 }
